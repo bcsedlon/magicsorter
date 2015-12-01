91107a32f4de5857a16e91aeeda27e2cd2912e92
bcsedlon <bcsedlon@gmail.com>
2015-12-01 20:04:09 +0100

    class reorganization

--- magicsorter/ms/management/commands/sort.py
+++ magicsorter/ms/management/commands/sort.py
@@ -18,7 +18,6 @@ import copy
 from ms.management.commands.modbus import ModbusServer
 #from PIL import Image    
 
-OUTBOX_NUM = 5
 THUMB_RATIO = 0.5
 THUMB_RATIO2 = 0.2
 
@@ -75,90 +74,288 @@ def t():
         #print pytesseract.image_to_string(im)
         #print(pytesseract.image_to_string(Image.open(filename), lang='eng'))
         return
-    
-SCAN = 0
-OUTBOX = 0
 
-def sorter():
-    global SCAN
-    global OUTBOX
-    
-    while SCAN != -1:
-        print 'Sorter: go to take card'
-        modbus.Scanner.outServo.value = 0
-        time.sleep(1)
-        #while modbus.Scanner.inServo.value != 0:
-        #    pass
-    
-        print 'Sorter: go to scan position'
-        modbus.Scanner.outServo.value = 100
-        time.sleep(1)
-        #while modbus.Scanner.inServo.value != 100:
-        #    pass
-    
-        print 'Sorter: scan card'
-        if SCAN != -1:
-            SCAN = 1
-            time.sleep(2)
-        
-        while OUTBOX==0:
-            pass
-        
-        #wait for out box
-        print 'Sorter: set out box {}'.format(OUTBOX)
-        modbus.Scanner.outFeeder.value = OUTBOX
-        time.sleep(1)
-        #while modbus.Scanner.inFeeder.value != OUTBOX:
-        #    pass
-        
-        print 'Sorter: go to release card'
-        modbus.Scanner.outServo.value = 120
-        time.sleep(1)
-        #while modbus.Scanner.inServo.value != 120:
-        #    pass
 
-#second round, no scan, only sort out
-def sorter2(outbox_num):
+class Sorter():
     
-    sortingout = []
+    outbox_num = 1
     
-    for s in Scan.objects.raw('SELECT id, fk_card_id, outbox, SUM(1) AS cnt FROM ms_scan WHERE position>0 GROUP BY fk_card_id ORDER BY cnt DESC'):
-        if len(sortingout) >=  outbox_num - 1:
-            break
+    RUN = True
+    #thr = None
+    
+    @staticmethod 
+    def init(outbox_num):
+        Sorter.outbox_num = outbox_num
+    
+    @staticmethod 
+    def sort():
+    
+        sortingout = []
+    
+        while Sorter.RUN:
+            print 'Sorter: go to take card'
+            modbus.Scanner.outServo.value = 0
+            time.sleep(1)
+            #while modbus.Scanner.inServo.value != 0:
+            #    pass
+    
+            print 'Sorter: go to scan position'
+            modbus.Scanner.outServo.value = 100
+            time.sleep(1)
+            #while modbus.Scanner.inServo.value != 100:
+            #    pass
+    
+            print 'Sorter: scan card'
+            scan = Scanner.scan()
         
-        print 'Sorter: card_id {} outbox {} count {}'.format(s.fk_card_id, s.outbox, s.cnt) 
-        sortingout.append(s.fk_card_id)
-         
-    #print sortingout 
- 
-    scans = Scan.objects.filter(position__gt=0).order_by('position')
+            if len(sortingout) <  Sorter.outbox_num - 1 and not scan.fk_card_id in sortingout:
+                sortingout.append(scan.fk_card_id)
+            #print sortingout
+        
+            if scan.fk_card_id in sortingout :
+                outbox = sortingout.index(scan.fk_card_id) + 1
+                scan.position = 0
+                scan.save()
+            else:
+                outbox = 0 
+        
+            print 'Sorter: set out box {} for card_id {}'.format(outbox, scan.fk_card_id)
+            modbus.Scanner.outFeeder.value = outbox
+            time.sleep(1)
+            #while modbus.Scanner.inFeeder.value != OUTBOX:
+            #    pass
+        
+            print 'Sorter: go to release card'
+            modbus.Scanner.outServo.value = 120
+            time.sleep(1)
+            #while modbus.Scanner.inServo.value != 120:
+            #    pass
+        
+
+    #@staticmethod    
+    #def start():
+    #    Sorter.thr = threading.Thread(target= Sorter.sorter, args=(), kwargs={})
+    #    Sorter.thr.start()
+
+    #@staticmethod    
+    #def stop():
+    #    Sorter.RUN = False
+    #    if Sorter.thr:
+    #        Sorter.thr.join()
+
+    #second round, no scan, only sort out
+    @staticmethod 
+    def sort2():
+    
+        sortingout = []
+    
+        for s in Scan.objects.raw('SELECT id, fk_card_id, outbox, SUM(1) AS cnt FROM ms_scan WHERE position>0 GROUP BY fk_card_id ORDER BY cnt DESC'):
+            if len(sortingout) >=  Sorter.outbox_num - 1:
+                break
+        
+            sortingout.append(s.fk_card_id)
+            print 'Sorter2: card_id {} count {} outbox {}'.format(s.fk_card_id, s.cnt, len(sortingout)) 
+                     
+        #print sortingout 
+        scans = Scan.objects.filter(position__gt=0).order_by('position')
      
-    for scan in scans:
+        for scan in scans:
             
-        print 'Sorter: go to take card'
-        modbus.Scanner.outServo.value = 0
-        time.sleep(1)
-        #while modbus.Scanner.inServo.value != 0:
-        #    pass
+            print 'Sorter2: go to take card'
+            modbus.Scanner.outServo.value = 0
+            time.sleep(1)
+            #while modbus.Scanner.inServo.value != 0:
+            #    pass
     
-        if scan.fk_card_id in sortingout :
-            outbox = sortingout.index(scan.fk_card_id) + 2
-            scan.position = 0
-            scan.save()
-        else:
-            outbox = 1 
+            
+            if scan.fk_card_id in sortingout :
+                outbox = sortingout.index(scan.fk_card_id) + 1
+                scan.position = 0
+                scan.save()
+            else:
+                outbox = 0 
           
-        print 'Sorter: set out box {} for card_id {}'.format(outbox, scan.fk_card_id)
-        modbus.Scanner.outFeeder.value = outbox
-        time.sleep(1)
-        #while modbus.Scanner.inFeeder.value != OUTBOX:
-        #    pass
+            print 'Sorter2: set out box {} for card_id {}'.format(outbox, scan.fk_card_id)
+            modbus.Scanner.outFeeder.value = outbox
+            time.sleep(1)
+            #while modbus.Scanner.inFeeder.value != OUTBOX:
+            #    pass
         
-        print 'Sorter: go to release card'
-        modbus.Scanner.outServo.value = 120
-        time.sleep(1) 
+            print 'Sorter2: go to release card'
+            modbus.Scanner.outServo.value = 120
+            time.sleep(1) 
+
+class Scanner():
+    
+    position = 0
+    outbox = 0
+    
+    text = None
+    
+    img = None
+    img1 = None
+    img2 = None
+    
+    RUN = True
+    thr = None
+    
+    @staticmethod 
+    def run():
+        cap = cv2.VideoCapture(1)
+        ret = cap.set(3, 1280)#320) 
+        ret = cap.set(4, 720)#240)
+        
+        ret, frame = cap.read()
+        #Scanner.img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        Scanner.img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        title = 'esc for quit'
+        cv2.namedWindow(title)
+        cv2.setMouseCallback(title, mouseclick)
+        
+        Scanner.img1 = None
+        
+        while Scanner.RUN:  
+            ret, frame = cap.read()
+            Scanner.img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            img0 = copy.copy(Scanner.img)
+         
+            if Scanner.img1 is not None:
+                height, width = Scanner.img1.shape 
+                img0[0:height, 0:width] = Scanner.img1
+             
+                if Scanner.img2 is not None:
+                    img0[height:height*2, 0:width]=Scanner.img2
+         
+            #cv2.putText(img0 , Scanner.text, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+            cv2.imshow(title, img0)
+            
+            k = cv2.waitKey(20) & 0xFF
+            if k == 27:
+                Scanner.RUN = False
+                Sorter.RUN = False
+                #Sorter.stop()
+                
+            #    break
+            
+            #print k
+            #if k == 113 or SCAN== -1:
+                # q
+            #    break
+        cap.release()
+        cv2.destroyAllWindows()  
+
+    @staticmethod    
+    def start():
+        Scanner.thr = threading.Thread(target= Scanner.run, args=(), kwargs={})
+        Scanner.thr.start()
+
+    @staticmethod    
+    def stop():
+        Scanner.RUN = False
+        
+        if Scanner.thr:
+            Scanner.thr.join()
     
     
+    @staticmethod    
+    def scan():
+        #OUTBOX = 0
+        #SCAN = 0
+    
+        Scanner.position = Scanner.position + 1
+                
+        img_hist = cv2.calcHist([Scanner.img],[0],None,[256],[0,256])
+        
+        card_id = 0
+        cards = Card.objects.order_by('pk')
+    
+        for card in cards:
+            template = cv2.imread(card.image.name, 1)
+            
+            if card.hist:
+                tempalte_hist = pickle.loads(card.hist)
+            else:
+                tempalte_hist = cv2.calcHist([template],[0],None,[256],[0,256])
+                card.hist = pickle.dumps(tempalte_hist)
+                card.save()
+            
+            #TODO compare images
+            #method = eval('CV_COMP_CORREL')
+            result = cv2.compareHist(img_hist, tempalte_hist, 0)
+            #print result
+        
+            if result > 0.99:
+                card_id = card.id
+                card.count = card.count + 1
+                card.save()
+                    
+                Scanner.img1 = cv2.imread(card.image.path, 0)
+                Scanner.img1 = cv2.resize(Scanner.img1, (0,0), fx=THUMB_RATIO2, fy=THUMB_RATIO2)   
+                Scanner.img2 = cv2.resize(Scanner.img, (0,0), fx=THUMB_RATIO2, fy=THUMB_RATIO2)   
+                    
+                Scanner.text = 'Scanner: match ' + str(card.id)
+                print Scanner.text
+                break
+        
+        #print card_id
+        if card_id == 0:
+            card = Card.objects.create()
+                
+            Scanner.img1 = cv2.resize(Scanner.img, (0,0), fx=THUMB_RATIO2, fy=THUMB_RATIO2)
+            Scanner.img2 = None
+                
+            Scanner.text = 'Scanner: new ' + str(card.id)
+            print Scanner.text
+                
+            card.count = 1
+            Scanner.outbox = Scanner.outbox + 1
+            card.outbox = Scanner.outbox
+            
+            card_id = card.id
+            card.hist = pickle.dumps(img_hist)
+                        
+            tmpfilename = '..jpg'             
+            cv2.imwrite(tmpfilename, Scanner.img)
+            f = open(tmpfilename, 'rb')
+            content = File(f)
+            card.image.save(str(card.id) + '.jpg', content)#, save=True)
+        
+            img = cv2.resize(Scanner.img, (0,0), fx=THUMB_RATIO, fy=THUMB_RATIO)   
+            cv2.imwrite(tmpfilename, img)
+            f = open(tmpfilename, 'rb')
+            content = File(f)
+            card.thumb.save(str(card.id) + '.jpg', content)
+            
+            card.save()
+            content.close()
+            f.close()
+            
+        scan = Scan.objects.create(position=Scanner.position, fk_card=card)
+        scan.outbox = card.outbox
+                
+        tmpfilename = '..jpg'   
+        img = cv2.resize(Scanner.img, (0,0), fx=THUMB_RATIO, fy=THUMB_RATIO)   
+        cv2.imwrite(tmpfilename, img)
+        f = open(tmpfilename, 'rb')
+        content = File(f)
+        scan.image.save(str(card.id) + '-'+ str(Scanner.position) + '.jpg', content)
+        content.close()
+        f.close()
+            
+        #print '{} / {}'.format(scan.outbox, OUTBOX_NUM)
+        #if scan.outbox < OUTBOX_NUM:
+        #    scan.position = 0
+        #        
+        #    OUTBOX = scan.outbox + 1
+        #else:
+            #1st outbox for notsorted cards
+        #    OUTBOX = 1
+        #OUTBOX = scan.outbox + 1
+        
+        scan.save()
+        return scan
+     
 
 def mouseclick(event,x,y,flags,param):
         #if event == cv2.EVENT_LBUTTONDBLCLK:
@@ -171,26 +368,14 @@ def mouseclick(event,x,y,flags,param):
 class Command(BaseCommand):
 
     help = 'magic sorter'
-    
-    
-    
-    
-        
-    global SCAN
-
-    
+     
     def handle(self, *args, **options):
 
-        
-
-        port = None
-        OUTBOX_NUM = 1
-        
         Config = ConfigParser.ConfigParser()
         try:
             Config.read('magicsorter.ini')
             port = Config.get('modbus', 'port')
-            OUTBOX_NUM = int(Config.get('sorter', 'outbox_num'))
+            outbox_num = int(Config.get('sorter', 'outbox_num'))
         except:
             cfgfile = open('magicsorter.ini','w+')
             # add the settings to the structure of the file, and lets write it out...
@@ -204,38 +389,44 @@ class Command(BaseCommand):
             print 'Please check magicsorter.ini and try again!'
             return
                 
-        global SCAN
-        global OUTBOX
-        
-    
-        
-        
         #ModbusServer.startServer()
         modbus.ModbusServer.init(port)
         modbus.ModbusServer.startServerAsync()
         
-        
-        
         #Card.objects.all().delete()
-        #cards = Card.objects.order_by('pk')
-        #for card in cards:
-        #    card.count = 0
-        #    card.outbox = 0
-        #    card.save()
         Card.objects.all().update(count=0, outbox=0)
         
-        #card = Card.objects.create()
-        #card.save()
+        Sorter.init(outbox_num)
         
         if Scan.objects.filter(position__gt=0):
-            sorter2(OUTBOX_NUM)
-            modbus.ModbusServer.stopServer()
-            return       
+            Sorter.sort2()
+        else:
+            
+            Scan.objects.all().delete()
         
-        thr = threading.Thread(target= sorter, args=(), kwargs={})
-        thr.start()
+            Scanner.start()  
+            #Sorter.start()
+            Sorter.sort()
         
-        Scan.objects.all().delete()
+            #while Scanner.RUN:
+            #    pass
+        
+            #Sorter.stop()
+            #Scanner.stop()
+        
+        modbus.ModbusServer.stopServer()
+        
+        return
+    
+############################################################################     
+        
+        #thr = threading.Thread(target= sorter, args=(), kwargs={})
+        #thr.start()
+        
+        
+        
+        
+        
         
         position = 0
         outbox = 0
